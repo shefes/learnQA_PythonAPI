7abf18f8ed216cca0fde0ae2b96e59e7a2a64cc5
shefes <125280689+shefes@users.noreply.github.com>
2023-02-14 16:37:28 +0800

    print hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+# import requests
+#
+# response = requests.get("https://playground.learnqa.ru/api/hello")
+# print(response.text)
+print("Hello from Sharefka")
